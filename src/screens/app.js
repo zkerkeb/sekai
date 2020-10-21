7a2b96457e54b7f0ccbcd0381efd0d25ce1bd9a9
Zakaria Kerkeb <hikkary@MacBook-Pro-de-Zakaria.local>
2020-10-21 17:27:09 +0200

    progression

---
+++ src/screens/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header'
+
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
